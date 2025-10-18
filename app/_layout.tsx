af667c46d6f45b691d1b709c18dcdfb23133274d
Abhinav Kumar Rao <119450947+abhinav-kr-rao@users.noreply.github.com>
2025-10-18 15:25:50 +0530

    added custom fonts and styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from 'expo-font'
 import "./global.css"
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [fontsLoaded, errors] = useFonts({
+    "Quicksand-Bold": require('@/assets/fonts/Quicksand-Bold.ttf'),
+    "Quicksand-Medium": require('@/assets/fonts/Quicksand-Medium.ttf'),
+    "Quicksand-Light": require('@/assets/fonts/Quicksand-Light.ttf'),
+    "Quicksand-Regular": require('@/assets/fonts/Quicksand-Regular.ttf'),
+    "Quicksand-Semibold": require('@/assets/fonts/Quicksand-SemiBold.ttf'),
+  })
+
+
+  useEffect(() => {
+    if (errors)
+      throw errors;
+    if (fontsLoaded)
+      SplashScreen.hideAsync();
+  }, [fontsLoaded, errors])
+
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold text-blue-600">
-        Welcome to Nativewind!
+      <Text className="text-4xl font-bold text-center font-quicksand-light text-red-600">
+        Welcome to my Nativewind!
       </Text>
     </View>
   );
